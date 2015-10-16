35f351c4b88e75985c238fde6dca28f31c537168
Adrian Godong <adrian.godong@gmail.com>
2015-10-16 12:10:51 -0700

    Expanded Client test assertions.

--- Medium.Tests/Client.cs
+++ Medium.Tests/Client.cs
@@ -18,6 +18,12 @@ namespace Medium.Tests
             var client = new Medium.Client();
             var user = client.GetCurrentUser(new Token { AccessToken = _accessToken });
             Assert.IsNotNull(user);
+
+            Assert.IsNotNull(user.Id);
+            Assert.IsNotNull(user.Username);
+            Assert.IsNotNull(user.Name);
+            Assert.IsNotNull(user.Url);
+            Assert.IsNotNull(user.ImageUrl);
         }
 
         [TestMethod]
@@ -26,8 +32,9 @@ namespace Medium.Tests
             var client = new Medium.Client();
             var title = "Liverpool FC";
             var tags = new[] { "football", "sport", "liverpool" };
+            var canonicalUrl = "http://jamietalbot.com/posts/liverpool-fc";
             var publishStatus = PublishStatus.Unlisted;
-            var license = License.PublicDomain;
+            var license = License.Cc40By;
 
             var author = client.GetCurrentUser(new Token { AccessToken = _accessToken });
             Assert.IsNotNull(author);
@@ -38,6 +45,7 @@ namespace Medium.Tests
                 ContentFormat = ContentFormat.Html,
                 Content = "<h1>Liverpool FC</h1><p>You’ll never walk alone.</p>",
                 Tags = tags,
+                CanonicalUrl = canonicalUrl,
                 PublishStatus = publishStatus,
                 License = license
             };
@@ -45,13 +53,17 @@ namespace Medium.Tests
             var post = client.CreatePost(author.Id, body, new Token { AccessToken = _accessToken });
             Assert.IsNotNull(post);
 
+            Assert.IsNotNull(post.Id);
             Assert.AreEqual(title, post.Title);
             Assert.AreEqual(author.Id, post.AuthorId);
             Assert.IsTrue(tags.All(post.Tags.Contains));
             Assert.IsTrue(post.Tags.All(tags.Contains));
+            Assert.IsNotNull(post.Url);
+            Assert.AreEqual(canonicalUrl, post.CanonicalUrl);
             Assert.AreEqual(publishStatus, post.PublishStatus);
             Assert.IsNotNull(post.PublishedAt);
             Assert.AreEqual(license, post.License);
+            // Assert.IsNotNull(post.LicenseUrl); // Looks like Medium does not return this field.
         }
 
         [TestMethod]
