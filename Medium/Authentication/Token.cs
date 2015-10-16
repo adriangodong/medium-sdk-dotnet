fc3a9925d96ad9b7c86fcf3742411b06ef8b7993
Adrian Godong <adrian.godong@gmail.com>
2015-10-16 12:09:21 -0700

    Added JSON field names to Token class.

--- Medium/Authentication/Token.cs
+++ Medium/Authentication/Token.cs
@@ -1,13 +1,24 @@
 using System;
+using Medium.Helpers;
+using Newtonsoft.Json;
 
 namespace Medium.Authentication
 {
     public class Token
     {
+        [JsonProperty(PropertyName = "token_type")]
         public string TokenType { get; set; }
+
+        [JsonProperty(PropertyName = "access_token")]
         public string AccessToken { get; set; }
+
+        [JsonProperty(PropertyName = "refresh_token")]
         public string RefreshToken { get; set; }
+
         public Scope[] Scope { get; set; }
+
+        [JsonProperty(PropertyName = "expires_at")]
+        [JsonConverter(typeof(UnixTimestampConverter))]
         public DateTime ExpiresAt { get; set; }
     }
 }
